d32867bd04483d921321c24c69b40b27cb224ba0
Aaron Ballard <mraballard@gmail.com>
2016-09-13 21:17:36 -0300

    mraballard -- Week 04 Project Blackjack
    Reset game works. Chip, deal, hit, stay buttons disable when game ends. Have only
    Bust functionality working. Need to add Stay, player win, push logic.
    Also need to add value of Player's stack to display.
    
    Page is very ugly.

--- blackjack.js
+++ blackjack.js
@@ -15,17 +15,36 @@ var App = {
   playerChipStack : 100,
   potValue : 0,
 
+  reset: function() {
+    //debugger;
+    App.deck = [];
+    App.potValue = 0;
+    App.playerCards = [];
+    App.dealerCards = [];
+    App.buildDeck();
+    UI.reset();
+    UI.showMessage('Place Your Bets!');
+  },
+
   buildDeck: function() {
+    App.hearts = [ {'2':2}, {'3':3}, {'4':4}, {'5':5}, {'6':6}, {'7':7}, {'8':8}, {'9':9}, {'10':10}, {'J': 10}, {'Q': 10}, {'K': 10}, {'A': 11} ];
+    App.diamonds = [ {'2':2}, {'3':3}, {'4':4}, {'5':5}, {'6':6}, {'7':7}, {'8':8}, {'9':9}, {'10':10}, {'J': 10}, {'Q': 10}, {'K': 10}, {'A': 11} ];
+    App.clubs = [ {'2':2}, {'3':3}, {'4':4}, {'5':5}, {'6':6}, {'7':7}, {'8':8}, {'9':9}, {'10':10}, {'J': 10}, {'Q': 10}, {'K': 10}, {'A': 11} ];
+    App.spades = [ {'2':2}, {'3':3}, {'4':4}, {'5':5}, {'6':6}, {'7':7}, {'8':8}, {'9':9}, {'10':10}, {'J': 10}, {'Q': 10}, {'K': 10}, {'A': 11} ];
     App.deck.push(App.hearts, App.diamonds, App.clubs, App.spades);
+    //debugger;
   },
 
   //create bet function to subtract bet from player's chips
   placeBet: function(chip) {
+    //debugger;
     if (App.playerChipStack < chip) {  //check if player has enough chips
       UI.showMessage('Not enough chips in your stack! Click "Deal"');
     } else {
+      console.log(chip);
       App.playerChipStack -= chip;
       App.potValue += chip;
+      console.log(App.potValue);
     }
     UI.showBet(App.potValue);
     UI.showMessage("Add to your Bet, or click 'Deal!'");
@@ -34,6 +53,7 @@ var App = {
 
   //create deal function
   dealCard: function() {
+    //debugger;
     var suit = Math.floor(Math.random()*4);  //0: hearts, 1: diamonds, 2: clubs, 3: spades
     var card = Math.floor(Math.random()*App.deck[suit].length);
     var dealtCard = [];
@@ -47,9 +67,11 @@ var App = {
 
   //initial deal function
   initialDeal: function() {
-    App.buildDeck();
+    // debugger;
+    App.reset();
     var initialDeal = [];
     for (var i = 0; i < 4; i++) {
+      console.log(i);
       initialDeal.push(App.dealCard());
       if (i%2 === 0) {
         App.playerCards.push(initialDeal[i]);
@@ -57,12 +79,30 @@ var App = {
         App.dealerCards.push(initialDeal[i]);
       }
     }
-    console.log(App.playerCards);
-    console.log(App.dealerCards);
-    // console.log(initialDeal);
-    console.log('going to showcard function');
-    UI.showCard(App.playerCards,App.dealerCards);
+    UI.deal(App.playerCards,App.dealerCards);
   },
+
+  calculateTotal: function(player) {
+    // debugger;
+    var total = 0;
+    player.forEach(function(card) {
+      total += card[0][Object.keys(card[0])];
+    });
+    return total;
+  },
+
+  //HIT
+  hit: function() {
+    // debugger;
+    App.playerCards.push(App.dealCard());
+    UI.addCard(App.playerCards[App.playerCards.length-1], 'playerCard');
+    if (App.calculateTotal(App.playerCards) > 21) {
+      App.bust('Player');
+    }
+
+  },
+
+
 
   //win function adds pot x2 to player's chips
   playerWins: function() {
@@ -70,8 +110,10 @@ var App = {
   },
 
   //lose function empties pot value
-  playerLoses: function() {
-
+  bust: function(player) {
+    //debugger;
+    UI.showMessage(player + ' busts!');
+    UI.endGame();
   },
 
   //push function returns pot value to player
@@ -88,6 +130,7 @@ var UI = {
   //chip click calls App.bet()
   onChipClick: function() {
     $('#chips').on('click', function(){
+      console.log('clicked chip');
       nickel = parseInt($('#chips').attr("data-set-id"));
       App.placeBet(nickel);
     });
@@ -104,7 +147,9 @@ var UI = {
 
   //hit click calls App.dealCard() to add card to player hand
   onHitButton: function() {
-
+    $('#hitButton').on('click', function(){
+      App.hit();
+    });
   },
 
   //stay click calls App.stay() which adds cards to dealer's hand until player score is beat or dealer busts
@@ -114,10 +159,19 @@ var UI = {
 
 //DOM manipulation
 
+  reset: function() {
+    $('#dealer').children().remove();
+    $('#player').children().remove();
+    $('#pot').text('');
+    $('#dealButton').attr("disabled", false);
+    $('#endGame').remove();
+    $('#chips').attr("disabled", false);
+
+  },
+
   //show "place your bets" in h1
   showMessage: function(message) {
     var $header = $('h1').text(message);
-    UI.onChipClick();
   },
 
   //show total of player's chips
@@ -133,7 +187,8 @@ var UI = {
   },
 
   //show dealt cards for player and dealer
-  showCard: function(playerCards, dealerCards) {
+  deal: function(playerCards, dealerCards) {
+    //debugger;
     for (var i = 0; i < playerCards.length; i++) {
       $newCard= Html.createCard(Object.keys(playerCards[i][0]),playerCards[i][1].suit,'playerCard');
       $('#player').append($newCard);
@@ -142,26 +197,35 @@ var UI = {
       $newCard = Html.createCard(Object.keys(dealerCards[i][0]),dealerCards[i][1].suit,'dealerCard');
       $('#dealer').append($newCard);
     }
+    $('#hitButton').attr("disabled", false);
+    $('#stayButton').attr("disabled", false);
+    $('#chips').attr("disabled", true);
   },
 
-  //add cards for hit on player or dealer
-  showHit: function(card) {
-
+  addCard: function(card, player) {
+    //debugger;
+    $newCard = Html.createCard(Object.keys(card[0]),card[1].suit, player);
+    if (player === 'playerCard') {
+      $('#player').append($newCard);
+    } else {
+      $('#dealer').append($newCard);
+    }
   },
 
-  //show win: display value of chips won
-  showWin: function() {
-
-  },
-
-  //show lose: display 'Dealer wins'
-  showLose: function() {
-
-  },
-  //show push: update value of player's chips
-  showPush: function() {
-
-  },
+  endGame: function() {
+    // debugger;
+    $('#hitButton').attr("disabled", true);
+    $('#stayButton').attr("disabled", true);
+    $resetButton = $('<button></button>');
+    $resetButton.attr('id','endGame');
+    $resetButton.text('Reset Game');
+    $('body').prepend($resetButton);
+    $resetButton.on('click', function() {
+      UI.reset();
+      App.reset();
+      UI.showMessage('Place Your Bets!');
+    });
+  }
 };
 
 /////////////////HTML
@@ -177,4 +241,7 @@ var Html = {
 //Initial Events
 window.onload = function() {
   UI.showMessage('Place Your Bets!');
+  UI.onChipClick();
+  UI.onHitButton();
+  UI.onStayButton();
 };
